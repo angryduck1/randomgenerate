79509cf3265f91d6e493e1688db25d0e5d3c926b
angryduck001 <98224800+angryduck1@users.noreply.github.com>
2024-08-11 21:15:19 +0300

    Add files via upload

--- random.py
+++ random.py
@@ -1,23 +1,31 @@
-import random 
-
-list = []
+import random  
+ 
+list = [] 
 rang = int(input("From 1 to: "))
-
-random_txt = open("random.txt", "w")
-
+count = int(input("Count: "))
+count_2 = 0
+ 
+random_txt = open("random.txt", "w") 
+ 
 def rand():
-  list.append(int(random.uniform(1, rang)))
-  random_txt.write(str(list) + "\n")
-  print(list)
- 
-while True: 
- keep_again = input("Generate? (y/n): ")
- 
- if keep_again == "n":
-  break
- elif keep_again == "y":
-  rand()
- else: 
-   print("Invalid command")
+  global count_2
+  while count_2 != count:
+    count_2 += 1 
+    list.append(int(random.uniform(1, rang))) 
+    print(list)
 
-random_txt.close()
+  random_txt.write(" ".join(map(str, list)))
+  
+while True:  
+ keep_again = input("Generate? (y/n): ") 
+  
+ if keep_again == "n": 
+  break 
+ elif keep_again == "y": 
+  rand() 
+ else:  
+   print("Invalid command") 
+ 
+random_txt.close() 
+
+#в данном коде я использовал нейросеть
