008debd3a7c92b85aaa917cb57176f6732ff9234
angryduck001 <98224800+angryduck1@users.noreply.github.com>
2024-08-12 19:31:36 +0300

    random.py

--- random.py
+++ random.py
@@ -1,31 +1,31 @@
-import random  
- 
+import random
+
+random_txt = open("random.txt", "w")
 list = [] 
-rang = int(input("From 1 to: "))
-count = int(input("Count: "))
-count_2 = 0
- 
-random_txt = open("random.txt", "w") 
  
 def rand():
   global count_2
   while count_2 != count:
     count_2 += 1 
-    list.append(int(random.uniform(1, rang))) 
+    list.append(int(random.uniform(rang, rang_2))) 
     print(list)
-
-  random_txt.write(" ".join(map(str, list)))
   
 while True:  
  keep_again = input("Generate? (y/n): ") 
   
  if keep_again == "n": 
+  random_txt.close() 
   break 
- elif keep_again == "y": 
-  rand() 
+ 
+ elif keep_again == "y":
+  rang = int(input("From: "))
+  rang_2 = int(input("To: "))
+  count = int(input("Count: "))
+  count_2 = 0
+
+  rand()
+  random_txt.write(str(list))
  else:  
    print("Invalid command") 
- 
-random_txt.close() 
 
 #в данном коде я использовал нейросеть
